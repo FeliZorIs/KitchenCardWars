6bf2ae9fdb4b0e361e20fbdf08ca20e3473578e5
Oris Feliz <felizoris@gmail.com>
2019-10-14 11:17:24 -0400

    made some progress on card droping. theyre able to start at certain position and come back to it when moved

--- Assets/Scripts/Dragable.cs
+++ Assets/Scripts/Dragable.cs
@@ -3,21 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class Dragable : MonoBehaviour, IDragHandler
+public class Dragable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
+    Transform parentToReturnTo = null;
+
     public void OnBeginDrag(PointerEventData eventData)
     {
         Debug.Log("OnBeginDrag");
+
+        parentToReturnTo = this.transform.parent;
+        this.transform.SetParent(this.transform.parent.parent);
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        Debug.Log("OnDrag");
+       // Debug.Log("OnDrag");
         this.transform.position = eventData.position;
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
         Debug.Log("OnEndDrag");
+        this.transform.SetParent(parentToReturnTo);
     }
 }
---
+++ Assets/Scripts/DropZone.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropZone : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler
+{
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log("OnDrop to: " + gameObject.name);
+    }
+}
