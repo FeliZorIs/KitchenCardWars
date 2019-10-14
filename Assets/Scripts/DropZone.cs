af57ec3fc62a19e79f142e436641d70787866f7c
Oris Feliz <felizoris@gmail.com>
2019-10-14 18:04:35 -0400

    got drag and drop to work to work... decently. not perfect, but when is anythiong perfect really

--- Assets/Scripts/DropZone.cs
+++ Assets/Scripts/DropZone.cs
@@ -5,18 +5,53 @@ using UnityEngine.EventSystems;
 
 public class DropZone : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler
 {
-    public void OnDrop(PointerEventData eventData)
-    {
-        Debug.Log("OnDrop to: " + gameObject.name);
-    }
+    public Dragable.Slot typeOfItem = Dragable.Slot.WEAPON;
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        Debug.Log("2");
+        if (eventData.pointerDrag == null)
+        {
+            return;
+        }
+
+        Dragable d = eventData.pointerDrag.GetComponent<Dragable>();
+        if (d != null)
+        {
+            if (typeOfItem == d.typeOfItem)
+            {
+                d.placeholderParent = this.transform;
+            }
+        }
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        Debug.Log("3");
+
+        if (eventData.pointerDrag == null)
+        {
+            return;
+        }
+
+        Dragable d = eventData.pointerDrag.GetComponent<Dragable>();
+        if (d != null && d.placeholderParent == this.transform)
+        {
+            if (typeOfItem == d.typeOfItem)
+            {
+                d.placeholderParent = d.parentToReturnTo;
+            }
+        }
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log(eventData.pointerDrag.name + " was dropped on to " + gameObject.name);
+
+        Dragable d = eventData.pointerDrag.GetComponent<Dragable>();
+        if (d != null)
+        {
+            if(typeOfItem == d.typeOfItem)
+                d.parentToReturnTo = this.transform;
+        }
     }
 }
+ 
