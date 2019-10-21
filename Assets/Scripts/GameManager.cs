7b15ef142d5d6f9ad733837bab270b3df2d6d052
Oris Feliz <felizoris@gmail.com>
2019-10-21 14:37:42 -0400

    added proper turn switching and at end of turn, the player's cards get empty and at start of trun, new cards are added

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,8 +6,14 @@ public class GameManager : MonoBehaviour
 {
     GameObject          enemy;
     GameObject          eventSystem;
+    Transform           hand;
+    public GameObject   Card;
 
     bool                turnSwitch;
+    bool                turnSwitch2;
+    bool                drawCards;
+
+    int          maxHandSize = 7;
 
     enum gameState
     {
@@ -18,8 +24,60 @@ public class GameManager : MonoBehaviour
     {
         GameState = gameState.PLAYERTURN;
         turnSwitch = false;
+        drawCards = false;
 
-        findObjects();
+        findObjects();    
+    }
+
+ //=======================================
+ //             Game Happenings
+ //=======================================
+    void Update()
+    {
+        switch (GameState)
+        {
+            case gameState.PLAYERTURN:
+                turnSwitch = false;
+                while (turnSwitch2 == false)                //Debug for checking turns
+                {
+                    Debug.Log("Player Turn");
+                    turnSwitch2 = true;
+                }
+
+                while (drawCards == false)
+                {
+                    newHand();
+                    drawCards = true;
+                }
+
+                eventSystem.gameObject.SetActive(true);
+                break;
+
+            case gameState.ENEMYTURN:
+                eventSystem.gameObject.SetActive(false);
+                turnSwitch2 = false;
+                drawCards = false;
+
+                while (turnSwitch == false)
+                {
+                    Debug.Log("Enemy Turn");
+                    StartCoroutine("wait");
+                    turnSwitch = true;
+                }
+                break;
+        }
+    }
+
+//========================================
+//              Functions
+//========================================
+
+    void findObjects()
+    {
+        enemy = GameObject.FindGameObjectWithTag("enemy");
+        eventSystem = GameObject.Find("EventSystem");
+        hand = GameObject.Find("Hand").transform;
+
         if (enemy == null)
             Debug.Log("This aint it Chief");
         else
@@ -30,46 +88,45 @@ public class GameManager : MonoBehaviour
         else
             Debug.Log("Ladies and gentlemen... we got " + eventSystem.gameObject.name);
 
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        switch (GameState)
-        {
-            case gameState.PLAYERTURN:
-                turnSwitch = false;
-                eventSystem.gameObject.SetActive(true);
-                break;
-
-            case gameState.ENEMYTURN:
-                eventSystem.gameObject.SetActive(false);
-                while (turnSwitch == false)
-                {
-                    Debug.Log("2");
-                    StartCoroutine("wait");
-                    turnSwitch = true;
-                }
-                break;
-        }
-    }
-
-    // Functions
-
-    void findObjects()
-    {
-        enemy = GameObject.FindGameObjectWithTag("enemy");
-        eventSystem = GameObject.Find("EventSystem");
+        if (hand == null)
+            Debug.Log("This aint it Chief");
+        else
+            Debug.Log("Ladies and gentlemen... we got " + hand.gameObject.name);
     }
 
     public void endTurn()
     {
+        emptyHand();
         GameState = gameState.ENEMYTURN;
     }
 
+    public void emptyHand()
+    {
+        int kidCount = hand.childCount;
+        Debug.Log(kidCount);
+
+        for (int i = 0; i < kidCount; i++)
+        {
+            Destroy(hand.GetChild(i).gameObject);
+            kidCount = hand.childCount;
+        }
+
+        Debug.Log(kidCount);
+    }
+
+    public void newHand()
+    {
+        int drawSize = maxHandSize;
+
+        for (int i = 1; i <= drawSize; i++)
+        {
+            Instantiate(Card, hand);
+        }
+    }
+    
     IEnumerator wait()
     {
-        yield return new WaitForSeconds(5f);
+        yield return new WaitForSeconds(3f);
         GameState = gameState.PLAYERTURN;
 
         yield return null;
