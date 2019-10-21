71cb3bb6ac8da6645c70ed799f35cd99c900872b
Oris Feliz <felizoris@gmail.com>
2019-10-21 18:08:08 -0400

    added small card script and folder for the cards

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Card : MonoBehaviour
+{
+    public int          attack;
+    public int          shield;
+    public int          health;
+    public int          cost;
+    public string       cardNameT;
+    public string       DescT;
+
+    Text cardName;
+    Text Desc;
+
+    void Start()
+    {
+        cardName = this.transform.FindChild("Name").GetComponent<Text>();
+        Desc = this.transform.FindChild("Description").GetComponent<Text>();
+
+        cardName.text = cardNameT;
+        Desc.text = DescT;
+    }
+}
